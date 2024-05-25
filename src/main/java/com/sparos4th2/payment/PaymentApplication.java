dbad52c94d7f68c1b73413054cf1f72714a6c983
이서용 <grang0224@g.kmou.ac.kr>
2024-05-26 03:54:59 +0900

    init

---
+++ src/main/java/com/sparos4th2/payment/PaymentApplication.java
@@ -0,0 +1,13 @@
+package com.sparos4th2.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
