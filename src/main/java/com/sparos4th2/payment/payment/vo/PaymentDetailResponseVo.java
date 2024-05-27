79b7fa9f59d1a8027fafc89d8f20584355f0db0e
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-05-27 10:26:44 +0900

    feat:결제내역조회 dto,vo 수정(#3)

--- src/main/java/com/sparos4th2/payment/payment/vo/PaymentDetailResponseVo.java
+++ src/main/java/com/sparos4th2/payment/payment/vo/PaymentDetailResponseVo.java
@@ -11,17 +11,19 @@ public class PaymentDetailResponseVo {
 	private String auctionUuid;
 	private String paymentMethod;
 	private int price;
+	private String paymentNumber;
 	private PaymentStatus paymentStatus;
 	private LocalDateTime paymentAt;
 	private LocalDateTime paymentCompletionAt;
 
 	public PaymentDetailResponseVo(String paymentUuid, String auctionUuid, String paymentMethod,
-		int price,
+		int price, String paymentNumber,
 		PaymentStatus paymentStatus, LocalDateTime paymentAt, LocalDateTime paymentCompletionAt) {
 		this.paymentUuid = paymentUuid;
 		this.auctionUuid = auctionUuid;
 		this.paymentMethod = paymentMethod;
 		this.price = price;
+		this.paymentNumber = paymentNumber;
 		this.paymentStatus = paymentStatus;
 		this.paymentAt = paymentAt;
 		this.paymentCompletionAt = paymentCompletionAt;
