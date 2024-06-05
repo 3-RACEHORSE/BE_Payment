8266e573670f19c1cd594341b1920156244b2552
ddolboghi <rhdfyd128@gmail.com>
2024-06-05 21:59:39 +0900

    refactor: MemberPaymentStatus에서 중복 문자열 제거

--- src/main/java/com/skyhorsemanpower/payment/common/MemberPaymentStatus.java
+++ src/main/java/com/skyhorsemanpower/payment/common/MemberPaymentStatus.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MemberPaymentStatus {
-	MEMBER_PAYMENT_AGREE,
-	MEMBER_PAYMENT_DISAGREE,
-	MEMBER_PAYMENT_READY
+	AGREE,
+	DISAGREE,
+	READY
 }
