29c020d63161aa7e09d50dcec2d7666dc6af4569
ddolboghi <rhdfyd128@gmail.com>
2024-06-05 19:48:43 +0900

    refactor: 결제 취소 상태 삭제

--- src/main/java/com/skyhorsemanpower/payment/common/PaymentStatus.java
+++ src/main/java/com/skyhorsemanpower/payment/common/PaymentStatus.java
@@ -7,9 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PaymentStatus {
 
-	PAYMENT_COMPLETE,
-	PAYMENT_FAIL,
-	PAYMENT_CANCEL,
-	PAYMENT_PENDING,
-	PAYMENT_READY
+	PAYMENT_COMPLETE, //결제 완료(판매자에게 지급)
+	PAYMENT_CANCEL, //결제 취소(둘 중에 한 명이라도 미동의 시)
+	PAYMENT_PENDING, //결제 대기(구매자가 결제)
+	PAYMENT_READY //입금 대기
 }
