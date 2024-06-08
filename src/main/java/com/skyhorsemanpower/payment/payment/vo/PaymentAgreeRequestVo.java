cb59b71f2666e137b2297beb919f3b8962ea3689
ddolboghi <rhdfyd128@gmail.com>
2024-06-09 03:04:21 +0900

    remove: 결제 승인, 결제 대기 여부 dto 및 vo 삭제

--- src/main/java/com/skyhorsemanpower/payment/payment/vo/PaymentAgreeRequestVo.java
+++
@@ -1,9 +0,0 @@
-package com.skyhorsemanpower.payment.payment.vo;
-
-import lombok.Getter;
-
-@Getter
-public class PaymentAgreeRequestVo {
-
-	private String paymentUuid;
-}
--- src/main/java/com/skyhorsemanpower/payment/payment/vo/PaymentExistResponseVo.java
+++
@@ -1,5 +0,0 @@
-package com.skyhorsemanpower.payment.payment.vo;
-
-public record PaymentExistResponseVo(boolean exist) {
-
-}
