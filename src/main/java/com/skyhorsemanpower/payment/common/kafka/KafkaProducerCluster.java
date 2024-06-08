72412d25c88269eb40b1f6016b229ad1ae9eedae
ddolboghi <rhdfyd128@gmail.com>
2024-06-09 05:56:23 +0900

    refactor: kafka producer 주석 제거

--- src/main/java/com/skyhorsemanpower/payment/common/kafka/KafkaProducerCluster.java
+++ src/main/java/com/skyhorsemanpower/payment/common/kafka/KafkaProducerCluster.java
@@ -11,25 +11,21 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class KafkaProducerCluster {
+
     private final KafkaTemplate<String, KafkaEntity> kafkaTemplate;
 
-    public void sendMessage(String topicName,KafkaEntity kafkaEntity) {
-//        Message<KafkaEntity> message = MessageBuilder
-//            .withPayload(kafkaEntity)
-//            .setHeader(KafkaHeaders.TOPIC, topicName)
-//            .build();
-
+    public void sendMessage(String topicName, KafkaEntity kafkaEntity) {
         CompletableFuture<SendResult<String, KafkaEntity>> future =
             kafkaTemplate.send(topicName, kafkaEntity);
 
         future.whenComplete((result, ex) -> {
             if (ex == null) {
                 log.info("producer: success >>> message: {}, offset: {}",
-                    result.getProducerRecord().value().toString(), result.getRecordMetadata().offset());
+                    result.getProducerRecord().value().toString(),
+                    result.getRecordMetadata().offset());
             } else {
                 log.info("producer: failure >>> message: {}", ex.getMessage());
             }
         });
-
     }
 }
