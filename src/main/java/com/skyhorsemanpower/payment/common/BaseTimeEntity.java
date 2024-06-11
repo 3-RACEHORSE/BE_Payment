8a2ce6b28054b62b3baa2a8061838664dd0ea5bb
ddolboghi <rhdfyd128@gmail.com>
2024-06-12 05:18:34 +0900

    fix: BaseTimeEntity updatedAt 필드 제거

--- src/main/java/com/skyhorsemanpower/payment/common/BaseTimeEntity.java
+++ src/main/java/com/skyhorsemanpower/payment/common/BaseTimeEntity.java
@@ -22,8 +22,4 @@ public class BaseTimeEntity {
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 }
