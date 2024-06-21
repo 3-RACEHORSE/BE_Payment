3daa81a5796af294b2b12b57f59618bf6d3ac7cb
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-21 16:15:20 +0900

    refactor: 경매 낙찰 시, 받는 토픽 변경(#47)

--- src/main/java/com/skyhorsemanpower/payment/kafka/KafkaConsumerCluster.java
+++ src/main/java/com/skyhorsemanpower/payment/kafka/KafkaConsumerCluster.java
@@ -23,18 +23,18 @@ public class KafkaConsumerCluster {
     private final PaymentService paymentService;
     private final KafkaProducerCluster producer;
 
-    @KafkaListener(topics = "successful-bid-topic", groupId = "${spring.kafka.consumer.group-id}")
+    @KafkaListener(topics = Topics.Constant.AUCTION_CLOSE, groupId = "${spring.kafka.consumer.group-id}")
     public void consumeBidder(@Payload LinkedHashMap<String, Object> message,
-        @Headers MessageHeaders messageHeaders) {
+                              @Headers MessageHeaders messageHeaders) {
         log.info("consumer: success >>> message: {}, headers: {}", message.toString(),
-            messageHeaders);
+                messageHeaders);
 
         //message를 PaymentReadyVo로 변환
         PaymentReadyVo paymentReadyVo = PaymentReadyVo.builder()
-            .auctionUuid(message.get("auctionUuid").toString())
-            .memberUuids((List<String>) message.get("memberUuids"))
-            .price(BigDecimal.valueOf((Integer) message.get("price")))
-            .build();
+                .auctionUuid(message.get("auctionUuid").toString())
+                .memberUuids((List<String>) message.get("memberUuids"))
+                .price(BigDecimal.valueOf((Integer) message.get("price")))
+                .build();
 
         log.info("consumer: success >>> paymentReadyVo: {}", paymentReadyVo.toString());
 
@@ -43,16 +43,16 @@ public class KafkaConsumerCluster {
 
     @KafkaListener(topics = "event-preview-topic", groupId = "${spring.kafka.consumer.group-id}")
     public void consumeEventPreview(@Payload LinkedHashMap<String, Object> message,
-        @Headers MessageHeaders messageHeaders) {
+                                    @Headers MessageHeaders messageHeaders) {
         String auctionUuid = message.get("auctionUuid").toString();
         List<PaymentListResponseDto> paymentListResponseDtos = paymentService.findCompletePayments(
-            auctionUuid);
+                auctionUuid);
         List<String> memberUuids = paymentListResponseDtos.stream()
-            .map(PaymentListResponseDto::getMemberUuid).toList();
+                .map(PaymentListResponseDto::getMemberUuid).toList();
 
         producer.sendMessage(Topics.NOTIFICATION_SERVICE.getTopic(), AlarmDto.builder()
-            .receiverUuids(memberUuids)
-            .eventType("payment")
-            .message("행사 시작까지 24시간 남았어요.").build());
+                .receiverUuids(memberUuids)
+                .eventType("payment")
+                .message("행사 시작까지 24시간 남았어요.").build());
     }
 }
